2d5606ef8c3a0ba02687ece75467762c5c9be509
sw-ye <2320233364@qq.com>
2025-08-04 01:40:21 +0800

    Initial commit

---
+++ src/main/java/com/chapter03/demo/com/itheima/domain/customer.java
@@ -0,0 +1,8 @@
+package com.chapter03.demo.com.itheima.domain;
+
+public class customer {
+    private Integer id;
+    private String content;
+    private String author;
+    private Integer aId;
+}
---
+++ src/main/java/com/chapter03/demo/com/itheima/domain/springbootdata.java
@@ -0,0 +1,10 @@
+package com.chapter03.demo.com.itheima.domain;
+
+import java.util.List;
+
+public class springbootdata {
+    private Integer id;
+    private String title;
+    private String content;
+    private List<customer> commentList;
+}
